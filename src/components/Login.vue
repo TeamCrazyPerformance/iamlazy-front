fa9d01dd346b04d331f0dd0149f05adf6de27926
joi0104 <joi0104@naver.com>
2020-03-18 23:39:13 +0900

    Add Kakao login
    Create Login vue component using Kakao login API and Register Login vue component on Home vue

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <img id="loginImg" src="//mud-kage.kakao.com/14/dn/btqbjxsO6vP/KPiGpdnsubSq3a0PHEGUK1/o.jpg"
+  v-on:click="loginWithKakao()"/>
+</template>
+
+<script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+
+<script>
+const kakaoAPI ='95883d28e87701fe86b707e0b9fc768a';
+
+export default {
+  name: 'Login',
+  methods: {
+    loginWithKakao() {
+      Kakao.init(kakaoAPI);
+      Kakao.Auth.login({
+        success: function(authObj) {
+          alert(JSON.stringify(authObj));
+        },
+        fail: function(err) {
+          alert('로그인에 실패했습니다.');
+        }
+      });
+    }
+  },
+};
+</script>
+
+<style>
+#loginImg {
+  width: 80%;
+}
+</style>
